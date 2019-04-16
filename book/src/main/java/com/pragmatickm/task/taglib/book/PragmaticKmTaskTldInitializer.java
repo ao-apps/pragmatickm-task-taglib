db958702b317b46f8646bb3b2d939c1aa855c89a
AO Industries, Inc <support@aoindustries.com>
2019-04-15 19:34:20 -0500

    Fixed cross-project linking of javadocs (really this time).
    The issue was our redirects from */apidocs/ to */apidocs/index.html
    were interacting with a Maven plugin bug:
    
    [MJAVADOC-539] Upgrading plugin from 3.0.0 to 3.0.1 and 3.1.0 breaks certain external javadoc links
    
    https://issues.apache.org/jira/browse/MJAVADOC-539

--- book/src/main/java/com/pragmatickm/task/taglib/book/PragmaticKmTaskTldInitializer.java
+++ book/src/main/java/com/pragmatickm/task/taglib/book/PragmaticKmTaskTldInitializer.java
@@ -31,12 +31,12 @@ public class PragmaticKmTaskTldInitializer extends TagReferenceInitializer {
 	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
 	static {
 		// Self
-		additionalApiLinks.put("com.pragmatickm.task.taglib.", Maven.properties.getProperty("documented.url") + "apidocs");
+		additionalApiLinks.put("com.pragmatickm.task.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/");
 		// Dependencies
-		additionalApiLinks.put("com.aoindustries.util.", "https://aoindustries.com/ao-lang/apidocs");
-		additionalApiLinks.put("com.pragmatickm.task.model.", "https://pragmatickm.com/task/model/apidocs");
-		additionalApiLinks.put("com.pragmatickm.task.servlet.", "https://pragmatickm.com/task/servlet/apidocs");
-		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs");
+		additionalApiLinks.put("com.aoindustries.util.", "https://aoindustries.com/ao-lang/apidocs/");
+		additionalApiLinks.put("com.pragmatickm.task.model.", "https://pragmatickm.com/task/model/apidocs/");
+		additionalApiLinks.put("com.pragmatickm.task.servlet.", "https://pragmatickm.com/task/servlet/apidocs/");
+		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs/");
 	}
 
 	public PragmaticKmTaskTldInitializer() {
@@ -45,7 +45,7 @@ public class PragmaticKmTaskTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/task/taglib",
 			"/pragmatickm-task.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk6"),
+			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			additionalApiLinks
 		);
