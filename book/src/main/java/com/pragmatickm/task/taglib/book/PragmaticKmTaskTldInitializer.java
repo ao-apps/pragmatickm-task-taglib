b2ff96ecbb2875d1b4f884033c60ad3fb22d5da4
AO Industries, Inc <support@aoindustries.com>
2020-05-30 00:25:27 -0500

    New option "requireLinks" that will fail when a class does not map to a
    package in "apiLinks".
    Defaults to "false".

--- book/src/main/java/com/pragmatickm/task/taglib/book/PragmaticKmTaskTldInitializer.java
+++ book/src/main/java/com/pragmatickm/task/taglib/book/PragmaticKmTaskTldInitializer.java
@@ -34,6 +34,7 @@ public class PragmaticKmTaskTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/task/taglib",
 			"/pragmatickm-task.tld",
+			true,
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
