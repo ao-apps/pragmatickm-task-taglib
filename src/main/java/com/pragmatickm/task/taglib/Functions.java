acf1412f753e4adde5e2acc2fb0f7b2901099f1e
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:49:05 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/pragmatickm/task/taglib/Functions.java
+++ src/main/java/com/pragmatickm/task/taglib/Functions.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-taglib - Tasks nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -38,7 +38,7 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletException;
 
-public abstract class Functions {
+public final class Functions {
 
 	/** Make no instances. */
 	private Functions() {throw new AssertionError();}
