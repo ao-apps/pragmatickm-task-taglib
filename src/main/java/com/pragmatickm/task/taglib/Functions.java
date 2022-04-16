efd7f3bea8e671bf5ef5a47701a2dea20a768472
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:11 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/task/taglib/Functions.java
+++ src/main/java/com/pragmatickm/task/taglib/Functions.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-task-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.task.taglib;
 
 import com.aoapps.lang.Strings;
